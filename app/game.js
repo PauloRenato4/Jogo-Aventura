554cd8139d98e0a60080ee3726c837bb04e0dac0
Paulo Renato Moreira Paschoal <paulo.dimaria34@hotmail.com>
2016-09-02 22:55:54 -0300

    modificações

--- app/game.js
+++ app/game.js
@@ -1,1 +1,4 @@
-var game = new Phaser.Game(1350, 668, Phaser.AUTO, "game-container");
+var Meugame = new Phaser.Game(1350, 668, Phaser.AUTO, "game-container");
+
+Meugame.state.add('game', gameState);
+Meugame.state.start('game');
